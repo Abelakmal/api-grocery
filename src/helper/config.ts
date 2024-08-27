eeb7a4ffb172e817df3dbe55d03663b62bbea115
abelakmal <abelakmal06@gmail.com>
2024-08-27 18:36:25 +0700

    fix cors

--- src/helper/config.ts
+++ src/helper/config.ts
@@ -2,6 +2,8 @@ export const PORT: number =
   (process.env.API_PORT as number | undefined) || 3000;
 export const baseURL = process.env.API_URL!;
 
+export const clientUrl = process.env.CLIENT_URL
+
 export const elasticsearchConfig = {
   node: process.env.ELASTICSEARCH_NODE || "http://localhost:9200",
 };
