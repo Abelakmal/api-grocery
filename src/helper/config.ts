631ac8367d4947d29738648d729b2522e177f51e
abelakmal <abelakmal06@gmail.com>
2024-08-31 22:57:13 +0700

    add volume for images

--- src/helper/config.ts
+++ src/helper/config.ts
@@ -2,7 +2,9 @@ export const PORT: number =
   (process.env.API_PORT as number | undefined) || 3000;
 export const baseURL = process.env.API_URL!;
 
-export const clientUrl = process.env.CLIENT_URL
+export const clientUrl = process.env.CLIENT_URL!;
+
+export const imgUploadPath = process.env.IMAGE_UPLOAD_PATH!;
 
 export const elasticsearchConfig = {
   node: process.env.ELASTICSEARCH_NODE || "http://localhost:9200",
